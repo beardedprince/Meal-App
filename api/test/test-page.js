71b764e6974b323b153d931429cd7d3f1d2428c7
beardedprince <alaketuolamilekan02@gmail.com>
2019-02-16 18:13:12 +0100

    endpoint tests

--- api/test/test-page.js
+++ api/test/test-page.js
@@ -6,4 +6,18 @@ it('Main page content', function(done) {
         expect(body).to.equal('the api is working fine now');
         done();
     });
-})
+});
+
+it('Main page status', function(done) {
+    request('http://localhost:9001/api/v1/meals' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+it('Main page status', function(done) {
+    request('http://localhost:9001/api/v1/meals/4' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
