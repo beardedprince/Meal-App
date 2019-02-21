7a2f29c0e95f8b92fcf48c2e15e63b58ad7dda81
beardedprince <alaketuolamilekan02@gmail.com>
2019-02-21 14:13:07 +0100

    Added nre code nase

--- api/test/test-page.js
+++ api/test/test-page.js
@@ -5,7 +5,6 @@ import request from 'request';
 
 chai.use(chaiHttp);
 
-
  it('Main page content', function(done) {
     request('http://localhost:9001' , function(error, response, body) {
          expect(body).to.equal('the api is working fine now');
