a84ccd3764e589e21e4fc1d993ca47237e3b8d11
beardedprince <alaketuolamilekan02@gmail.com>
2019-02-21 12:48:39 +0100

    fixing travis

--- api/test/test-page.js
+++ api/test/test-page.js
@@ -1,24 +1,28 @@
-const expect = require('chai').expect;
-const request = require('request');
-import nyc from 'nyc';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../index';
+import request from 'request';
 
-it('Main page content', function(done) {
+chai.use(chaiHttp);
+
+
+ it('Main page content', function(done) {
     request('http://localhost:9001' , function(error, response, body) {
-        expect(body).to.equal('the api is working fine now');
-        done();
-    });
-});
+         expect(body).to.equal('the api is working fine now');
+         done();
+     });
+ });
 
-// it('Main page status', function(done) {
-//     request('http://localhost:9001/api/v1/meals' , function(error, response, body) {
-//         expect(response.statusCode).to.equal(200);
-//         done();
-//     });
-// });
+// // it('Main page status', function(done) {
+// //     request('http://localhost:9001/api/v1/meals' , function(error, response, body) {
+// //         expect(response.statusCode).to.equal(200);
+// //         done();
+// //     });
+// // });
 
-// it('Main page status', function(done) {
-//     request('http://localhost:9001/api/v1/meals/4' , function(error, response, body) {
-//         expect(response.statusCode).to.equal(200);
-//         done();
-//     });
-// });
+// // it('Main page status', function(done) {
+// //     request('http://localhost:9001/api/v1/meals/4' , function(error, response, body) {
+// //         expect(response.statusCode).to.equal(200);
+// //         done();
+// //     });
+// // });
