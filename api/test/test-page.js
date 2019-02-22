95cf1783353be70918dca2944e184574a1ce4d09
beardedprince <alaketuolamilekan02@gmail.com>
2019-02-22 10:31:09 +0100

    fix #001

--- api/test/test-page.js
+++ api/test/test-page.js
@@ -1,27 +1,15 @@
-import chai, { expect } from 'chai';
+import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index';
-import request from 'request';
+
 
 chai.use(chaiHttp);
 
- it('Main page content', function(done) {
-    request('http://localhost:9001' , function(error, response, body) {
-         expect(body).to.equal('the api is working fine now');
-         done();
-     });
- });
+describe('Meal', () => {
+  beforeEach((done) => {
+    done();
+  });
 
-// // it('Main page status', function(done) {
-// //     request('http://localhost:9001/api/v1/meals' , function(error, response, body) {
-// //         expect(response.statusCode).to.equal(200);
-// //         done();
-// //     });
-// // });
 
-// // it('Main page status', function(done) {
-// //     request('http://localhost:9001/api/v1/meals/4' , function(error, response, body) {
-// //         expect(response.statusCode).to.equal(200);
-// //         done();
-// //     });
-// // });
+
+});
