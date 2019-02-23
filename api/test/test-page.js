c690a7281fb5e8f763749f38aaafed63e428027e
beardedprince <alaketuolamilekan02@gmail.com>
2019-02-24 00:11:21 +0100

    added test branch with passes test

--- api/test/test-page.js
+++ api/test/test-page.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import 'chai/register-should';
 import app from '../index';
 
 
@@ -10,6 +11,21 @@ describe('Meal', () => {
     done();
   });
 
+/**
+   * Test the GET /meals/ route
+   */
+  describe('GET /meals', () => {
+    it('it should get all the meals', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/meals')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.data.should.be.a('array');
+          done();
+        });
+    });
+  });
 
 
 });
