05b925c85d6532381bec87f581678050f07c1d5e
beardedprince <alaketuolamilekan02@gmail.com>
2019-02-25 15:54:48 +0100

    All test now working

--- api/test/meal.test.js
+++ api/test/meal.test.js
@@ -1,52 +1,45 @@
+process.env.NODE_ENV = 'test';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import 'chai/register-should';
-import app from '../index';
-import dummyData from '../utils/dummydata';
+import index from '../index';
+const should = chai.should();
+import dummyData from '../utils/dummyData';
 
 
 chai.use(chaiHttp);
 
-describe('Meal', () => {
-  beforeEach((done) => {
-    done();
+/*
+  * Test the GET /meals/ route
+  */
+  describe('GET Meals', () => {
+      it('it should GET all the Meals', (done) => {
+        chai.request(index)
+            .get('/api/v1/meals')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.data.should.be.a('array');
+              done();
+            });
+      });
   });
 
-  /**
-   * Test the GET /meals/ route
-   */
-  describe('GET /meals', () => {
-    it('it should get all the meals', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/meals')
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.data.should.be.a('array');
-          done();
-        });
-    });
-  });
-
-
-  /**
+/**
    * Test the POST /meals/ route
    */
   describe('POST /meals', () => {
-        it('it should post a meal', (done) => {
+    it('it should post a meal', (done) => {
       const meal = {
         name: 'Porridge',
         size: 'Small',
         price: '300',
       };
       chai
-        .request(app)
+        .request(index)
         .post('/api/v1/meals')
         .send(meal)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
-          
           res.body.data.should.have.property('id');
           res.body.data.should.have.property('name');
           res.body.data.should.have.property('size');
@@ -57,4 +50,45 @@ describe('Meal', () => {
   });
 
 
-});
+  /**
+   * Test the GET /meals/:id route
+   */
+  describe('GET /meals/:id', () => {
+    it('it should GET a meal by the given id', (done) => {
+      const mealId = Number(dummyData.meals[0].id);
+      chai
+        .request(index)
+        .get(`/api/v1/meals/${mealId}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.data.should.have.property('id').eql(mealId);
+          res.body.data.should.have.property('name');
+          res.body.data.should.have.property('size');
+          res.body.data.should.have.property('price');
+          done();
+        });
+    });
+  });
+
+
+
+  
+  /**
+   * Test  DELETE /meals/:id route
+   */
+  describe('DELETE /meal/:id', () => {
+    it('it should delete a meal given the id', (done) => {
+      const mealId = Number(dummyData.meals[0].id);
+
+      chai
+        .request(index)
+        .delete(`/api/v1/meals/${mealId}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
+  });
